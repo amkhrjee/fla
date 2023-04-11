15c1bddddfdb2f2e9cd43fd816a6a13377b6c663
amkhrjee <aniruddha.mukherji@outlook.com>
2023-04-12 00:18:56 +0530

    added documentation

--- dfa_to_mindfa.py
+++ dfa_to_mindfa.py
@@ -2,7 +2,12 @@
 #   Author: Aniruddha Mukherjee         #
 #   Roll No. CSB21076                   #
 #########################################
-from beautifultable import BeautifulTable as btable
+
+# Note: by default table formatting is disabled.
+# after adding beautifultable via `pip install beautifultable` to the environment
+# you can uncomment the relevant portions to see the formatted table as the output
+
+# from beautifultable import BeautifulTable as btable
 
 dfa_transition_table = {
     'a': {'0': 'b', '1': 'c'},
@@ -34,19 +39,26 @@ def is_equal(item_a, item_b):
 
 
 def get_equivalent_states(transition_table, prev_equivalence_list, equivalence_list):
+    # base case: checks equality of nested lists
     if is_equal(prev_equivalence_list, equivalence_list):
         return equivalence_list
     else:
+        # creates new list containing the new equivalence classes
         temp_list = []
         new_list = []
         temp_state_for_input_zero = ''
         temp_state_for_input_one = ''
+        # goes through each equivalence class in the list
         for group in equivalence_list:
             curr_group = group
+            # used to check whether two members have the same transition state for the same input
             temp_state_for_input_zero = transition_table[group[0]]['0']
             temp_state_for_input_one = transition_table[group[0]]['1']
+            # checks for each member of the equivalence class
             for member in group:
+                # checks whether the transition state belongs to the same equivalence class
                 for input in transition_table[member]:
+                    # checks whether the transition state on input are the same for the members
                     if input == '0':
                         if transition_table[member][input] != temp_state_for_input_zero:
                             curr_group.remove(member)
@@ -63,13 +75,17 @@ def get_equivalent_states(transition_table, prev_equivalence_list, equivalence_l
             transition_table, equivalence_list, sorted(new_list))
 
 
-def dfa_to_mindfa(transition_table, start_state, end_states):
+def dfa_to_mindfa(dfa_transition_table, start_state, end_states):
+    transition_table = dfa_transition_table
+    # the final list of equivalence classes
     equivalence_lists = [
         [x for x in transition_table if x not in end_states], [x for x in end_states]]
     equivalent_list = get_equivalent_states(
         transition_table, [], equivalence_lists)
     new_state_str_list = []
+    # goes through each equivalence class
     for group in equivalent_list:
+        # makes new state and replaces the equivalent states
         new_state_str = "".join(sorted(group))
         new_state_str_list.append(new_state_str)
         for member in group:
@@ -80,7 +96,9 @@ def dfa_to_mindfa(transition_table, start_state, end_states):
                     input_zero = each_str
                 if transition_table[member]['1'] in each_str:
                     input_one = each_str
+            # removes the old states
             del transition_table[member]
+        # adds the new state
         transition_table[new_state_str] = {
             '0': "".join(sorted(input_zero)),
             '1': "".join(sorted(input_one))}
@@ -90,18 +108,30 @@ def dfa_to_mindfa(transition_table, start_state, end_states):
 # test
 dfa_table, start_state, end_states = dfa_to_mindfa(
     dfa_transition_table, 'a', ['e'])
-table = btable()
 
+# table = btable()
+# for state in dfa_table:
+#     if start_state in state:
+#         table.rows.append(
+#             ["-> " + state, dfa_table[state]['0'], dfa_table[state]['1']])
+#     elif state in end_states:
+#         table.rows.append(
+#             ["* " + state, dfa_table[state]['0'], dfa_table[state]['1']])
+#     else:
+#         table.rows.append(
+#             [state, dfa_table[state]['0'], dfa_table[state]['1']])
+
+# table.columns.header = ["States", "0", "1"]
+# print(table)
+
+# without formatting:
 for state in dfa_table:
     if start_state in state:
-        table.rows.append(
-            ["-> " + state, dfa_table[state]['0'], dfa_table[state]['1']])
+        print("-> " + state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
     elif state in end_states:
-        table.rows.append(
-            ["* " + state, dfa_table[state]['0'], dfa_table[state]['1']])
+        print("* " + state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
     else:
-        table.rows.append(
-            [state, dfa_table[state]['0'], dfa_table[state]['1']])
-
-table.columns.header = ["States", "0", "1"]
-print(table)
+        print(state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -2,7 +2,12 @@
 #   Author: Aniruddha Mukherjee         #
 #   Roll No. CSB21076                   #
 #########################################
-from beautifultable import BeautifulTable as btable
+
+# Note: by default table formatting is disabled.
+# after adding beautifultable via `pip install beautifultable` to the environment
+# you can uncomment the relevant portions to see the formatted table as the output
+
+# from beautifultable import BeautifulTable as btable
 
 nfa_transition_table = {
     'q0': {'0': ['q0'], '1': ['q0', 'q1']},
@@ -12,16 +17,21 @@ nfa_transition_table = {
 
 
 def nfa_to_dfa(transition_table, start_state, end_states):
+    # initialization
     dfa_transition_table = {}
     untracked_states = [start_state]
     for curr_state in untracked_states:
+        # checks if there are multiple states
         if isinstance(curr_state, list):
             curr_state_str = "".join(sorted(curr_state))
+            # declaring as sets for performing union
             states_for_zero = set()
             states_for_one = set()
+            # performing union for each input
             for state in curr_state:
                 states_for_zero.update(transition_table[state].get('0', []))
                 states_for_one.update(transition_table[state].get('1', []))
+            # producing the strings for the table
             if len(states_for_zero):
                 zero_str = "".join(sorted(states_for_zero))
             else:
@@ -31,37 +41,44 @@ def nfa_to_dfa(transition_table, start_state, end_states):
                 one_str = "".join(sorted(states_for_one))
             else:
                 one_str = 'z'
-
+            # adding the strings to the table
             dfa_transition_table[curr_state_str] = {
                 '0': zero_str,
                 '1': one_str
             }
-
+            # adding the new states encountered to our list of untracked states
             if sorted(list(states_for_zero)) not in untracked_states:
                 untracked_states.append((sorted(list(states_for_zero))))
             if sorted(list(states_for_one)) not in untracked_states:
                 untracked_states.append(sorted(list(states_for_one)))
+        # dealing with empty states
         elif curr_state == 'z':
             dfa_transition_table['z'] = {'0': 'z', '1': 'z'}
+        # dealing with single state
         else:
             dfa_transition_table.update({curr_state: {'0': None, '1': None}})
+            # dealing with each input
             for symbol in transition_table[curr_state]:
                 target_states = transition_table[curr_state][symbol]
+                # for empty states
                 if len(target_states) == 0:
                     dfa_transition_table[curr_state][symbol] = 'z'
                     if 'z' not in untracked_states:
                         untracked_states.append('z')
+                # for single states on input
                 elif len(target_states) == 1:
                     state_str = "".join(target_states)
                     dfa_transition_table[curr_state][symbol] = state_str
                     if state_str not in untracked_states:
                         untracked_states.append(state_str)
+                # for multiple states on input
                 else:
                     target_state_str = "".join(sorted(target_states))
                     dfa_transition_table[curr_state][symbol] = target_state_str
                     if sorted(target_states) not in untracked_states:
                         untracked_states.append(sorted(target_states))
     dfa_end_states = []
+    # dealing with end states
     for each_end_state in end_states:
         for each_state in dfa_transition_table:
             if each_end_state in each_state:
@@ -72,19 +89,30 @@ def nfa_to_dfa(transition_table, start_state, end_states):
 # test
 dfa_table, start_state, end_states = nfa_to_dfa(
     nfa_transition_table, 'q0', ['q2'])
-table = btable()
 
+# table = btable()
+# for state in dfa_table:
+#     if state is start_state:
+#         table.rows.append(
+#             ["->" + state, dfa_table[state]['0'], dfa_table[state]['1']])
+#     elif state in end_states:
+#         table.rows.append(
+#             ["*" + state, dfa_table[state]['0'], dfa_table[state]['1']])
+#     else:
+#         table.rows.append(
+#             [state, dfa_table[state]['0'], dfa_table[state]['1']])
 
+# table.columns.header = ["States", "0", "1"]
+# print(table)
+
+# without formatting:
 for state in dfa_table:
-    if state is start_state:
-        table.rows.append(
-            ["->" + state, dfa_table[state]['0'], dfa_table[state]['1']])
+    if state in start_state:
+        print("-> " + state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
     elif state in end_states:
-        table.rows.append(
-            ["*" + state, dfa_table[state]['0'], dfa_table[state]['1']])
+        print("* " + state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
     else:
-        table.rows.append(
-            [state, dfa_table[state]['0'], dfa_table[state]['1']])
-
-table.columns.header = ["States", "0", "1"]
-print(table)
+        print(state + " = " + "['0' : " + dfa_table[state]
+              ['0'] + ", '1' : " + dfa_table[state]['1'] + "]")
